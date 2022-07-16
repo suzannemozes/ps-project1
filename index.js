8e92c17409b2e6e665b207d251c9b1269bdc9828
Suzanne Mozes <suzanne.mozes@gmail.com>
2022-07-16 17:40:18 -0400

    dice in place on board

--- index.js
+++ index.js
@@ -2,26 +2,25 @@
 // // Dice start at 00
 // // Let player pick color and computer takes other
 
-// let player = 'Suzanne';
-// let computer = 'Computer';
-// let dieWinner;
+let player = 'Suzanne';
+let computer = 'Computer';
+let dieWinner;
 
 // // const bluepath = [div1, div2, div3, div4, div5, div6, div7, div8, div9, div10, div11, div12, div13, divhome];
 
 // // const diceA = ["1", "2", "3", "4", "5", "6"];//use these two arrays to loop to get die to visually show up on the screen
 // // const diceB = ["1", "2", "3", "4", "5", "6"];
 
-// //BUTTON
-// // const button = document.querySelector("button");
+//BUTTON
+const button = document.querySelector("button");
 
 // //DICE with with PNGS
 const firstRandomNum = Math.floor((Math.random()*6) + 1);
-
-
-// // const dice = [ //this is an object with two arrays
-// //   diceA = ["1", "2", "3", "4", "5", "6"];//use these two arrays to loop to get die to visually show up on the screen
-// //   diceB = ["1", "2", "3", "4", "5", "6"]
-// // ] 
+const firstDiceImage = 'assets/dice' + firstRandomNum + '.png';
+dice1 = document.querySelectorAll('img')[0].setAttribute('src', firstDiceImage);
+const secondRandomNum = Math.floor((Math.random()*6) + 1);
+const secondDiceImage = 'assets/dice' + secondRandomNum + '.png';
+dice2 = document.querySelectorAll('img')[1].setAttribute('src', secondDiceImage);
 
 // // visual movement of parcheesi pieces
 // // use array.find callback to cycle through divs. Returns 1st matching value and only one value is returned.
@@ -30,34 +29,34 @@ const firstRandomNum = Math.floor((Math.random()*6) + 1);
 // // })
 
 // ///
-// function game(){ //LOGIC KEEP ALL PLAY INSIDE GAME FUNCTION WITH NESTED ARRAY / OBJECTS B/C BOTH NEED TO HAVE THE SAME REFERENCE TYPE AKA ROOT??
+function game(){ //LOGIC KEEP ALL PLAY INSIDE GAME FUNCTION WITH NESTED ARRAY / OBJECTS B/C BOTH NEED TO HAVE THE SAME REFERENCE TYPE AKA ROOT??
  
-//   function rollDie(){
-//     let dieWinner;
-//     let playerRoll = Math.floor((Math.random()*6)+1);
-//     let computerRoll = Math.floor((Math.random()*6)+1);
-//     if (playerRoll < computerRoll){
-//       dieWinner = player;
-//       console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${player} plays first.`);
-//     }
-//     else if (playerRoll > computerRoll) {
-//       dieWinner = computer;
-//       console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${computer} plays first.`);
-//       }
-//     else {
-//       console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. We don't have a winner. Roll again, and let's see who goes first.`);
-//         //rollDie(); this code works but the automation is instant without letting the player interact //BUTTON THAT SAYS ROLL AGAIN?
-//     }
-//     //ADD LOOP (SWITCH STATEMENT?) OF ARRAY diceA to reflect the actual visual image of the number rolled 
-//     //playerRoll shows up on dice[A] and later on with both dice[A] and dice[b]
-//   }
+  function rollDie(){
+    let dieWinner;
+    let playerRoll = Math.floor((Math.random()*6)+1);
+    let computerRoll = Math.floor((Math.random()*6)+1);
+    if (playerRoll < computerRoll){
+      dieWinner = player;
+      console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${player} plays first.`);
+    }
+    else if (playerRoll > computerRoll) {
+      dieWinner = computer;
+      console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${computer} plays first.`);
+      }
+    else {
+      console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. We don't have a winner. Roll again, and let's see who goes first.`);
+        //rollDie(); this code works but the automation is instant without letting the player interact //BUTTON THAT SAYS ROLL AGAIN?
+    }
+    //ADD LOOP (SWITCH STATEMENT?) OF ARRAY diceA to reflect the actual visual image of the number rolled 
+    //playerRoll shows up on dice[A] and later on with both dice[A] and dice[b]
+  }
   
-//   rollDie(player, computer);
+  rollDie(player, computer);
   
-//   function throwDice(){//dieWinner rolls two dice //CSS shakes the cup
+  function throwDice(){//dieWinner rolls two dice //CSS shakes the cup
 
-// }
-// game();
+}}
+game();
 
 // // function() {
 // //   const diceA = ["1", "2", "3", "4", "5", "6"];//use these two arrays to loop to get die to visually show up on the screen
@@ -90,7 +89,7 @@ const firstRandomNum = Math.floor((Math.random()*6) + 1);
 
 //   //if you throw doubles, you immediately take another turn
 
-//   //Parcheesi rules specifiy, two pieces of the same color that have stopped (by exact count) on the same space (cream or blue, including your ‘starting space’) form a blockade. No pieces may pass this blockade, including your own. You must move spaces equal to the die roll on each turn, if able. You can leave a blockade in place until you cannot move any other pieces, but you must break the blockade if you have no other moves.
+//   //Parcheesi rules specify, two pieces of the same color that have stopped (by exact count) on the same space (cream or blue, including your ‘starting space’) form a blockade. No pieces may pass this blockade, including your own. You must move spaces equal to the die roll on each turn, if able. You can leave a blockade in place until you cannot move any other pieces, but you must break the blockade if you have no other moves.
 
 //   //A piece only enters the ‘Home’ square by exact count. If you are 2 spaces from the ‘Home’ square and roll a 4 and 5, you cannot move that piece.
 //   //to win the game, be the first to move all your pieces to the center ‘Home’ square. 
