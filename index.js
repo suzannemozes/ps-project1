0344003dd9095fa54ce0cd8517c7a521eceeee59
Suzanne Mozes <suzanne.mozes@gmail.com>
2022-07-19 21:22:21 -0400

    expanded to project1b and stylesheetb

--- index.js
+++ index.js
@@ -5,7 +5,12 @@
 // // Opening line to tell player to roll dice and the  player with the lowest roll goes first.
 // // Dice start at 00
 // // Let player pick color and computer takes other
-
+const firstRandomNum = Math.floor((Math.random()*6)+1);
+const firstDiceImage = 'assets/dice' + firstRandomNum + '.png';
+dice1 = document.querySelector('#dicea').setAttribute('src', firstDiceImage);
+const secondRandomNum = Math.floor((Math.random()*6) + 1);
+const secondDiceImage = 'assets/dice' + secondRandomNum + '.png';
+dice2 = document.querySelector('#diceb').setAttribute('src', secondDiceImage);
 let player = 'Player1';
 let computer = 'Computer';
 let dieWinner;
@@ -22,7 +27,7 @@ let dieWinner;
 // peachPieces = [peachPiece1, peachPiece2, peachPiece3, peachPiece4];
 
 
-const bluePath = [bluenest, div0, div1, div2, div3, div4, div5, div7, div8, div9, div10, div11, div12, divhome];
+// const bluePath = [bluenest, div0, div1, div2, div3, div4, div5, div7, div8, div9, div10, div11, div12, divhome];
 // const yellowPath = [div15, div16, div17, div18, div19, div20, div7, div8, div9, div10, div11, div12, div14, divhome]
 
 //BUTTON
@@ -31,57 +36,8 @@ const button = document.querySelector("button");
 
 // dice with with .pngs
 // create opening scene with empty dice, and then a single dice roll to see who starts, and then onto both, const dicestartgame = src .. .dice0??
-const firstRandomNum = Math.floor((Math.random()*6) + 1);
-const firstDiceImage = 'assets/dice' + firstRandomNum + '.png';
-dice1 = document.querySelector('#dicea').setAttribute('src', firstDiceImage);
-const secondRandomNum = Math.floor((Math.random()*6) + 1);
-const secondDiceImage = 'assets/dice' + secondRandomNum + '.png';
-dice2 = document.querySelector('#diceb').setAttribute('src', secondDiceImage);
-
-//parcheesi pieces
-const bluePiece1 = document.querySelector('#bluePiece1');
-const bluePiece2 = document.querySelector('#bluePiece2');
-const bluePiece3 = document.querySelector('#bluePiece3');
-const bluePiece4 = document.querySelector('#bluePiece4');
-
-const playerpieces = document.querySelector('.playerpieces');
-playerpieces.addEventListener('mouseover', function(){
-  document.getElementById("playerpieces").style.backgroundColor = "green";
-});
 
 
-const peachPiece1 = document.querySelector('#peachPiece1');
-const peachPiece2 = document.querySelector('#peachPiece2');
-const peachPiece3 = document.querySelector('#peachPiece3');
-const peachPiece4 = document.querySelector('#peachPiece4');
-
-const redPiece1 = document.querySelector('#redPiece1');
-const redPiece2 = document.querySelector('#redPiece2');
-const redPiece3 = document.querySelector('#redPiece3');
-const redPiece4 = document.querySelector('#redPiece4');
-
-const yellowPiece1 = document.querySelector('#yellowPiece1');
-const yellowPiece2 = document.querySelector('#yellowPiece2');
-const yellowPiece3 = document.querySelector('#yellowPiece3');
-const yellowPiece4 = document.querySelector('#yellowPiece4');
-
-const allPieces = document.getElementsByClassName('.playerpieces')
-
-let bluePieces = [bluePiece1, bluePiece2, bluePiece3, bluePiece4];
-let yellowPieces = [yellowPiece1, yellowPiece2, yellowPiece3, yellowPiece4];
-let redPieces = [redPiece1, redPiece2, redPiece3, redPiece4];
-let peachPieces = [peachPiece1, peachPiece2, peachPiece3, peachPiece4];
-// const bluePiece = document.querySelector('b-piece-area').setAttribute('src', darkbluepiece.svg)
-// const yellowPiece = document.querySelectory('y-piece-area').setAttribute('src', yellowpiece.svg)
-// const peachPiece = document.querySelectory('p-piece-area').setAttribute('src', peachpiece.svg)
-// const redPiece = document.querySelectory('r-piece-area').setAttribute('src', redpiece.svg)
-// // use array.find callback to cycle through divs. Returns 1st matching value and only one value is returned.
-// // bluepath.find(bpath => {
-// //   return bluepath.includes('nextdiv');
-// // })
-
-// ///
-//to update pieces on its own
 const pawns = {
   nest: [],
   onPath: [],
@@ -100,6 +56,7 @@ const pawns = {
     }
     return nest;
   },
+
   drawPawn() {
     const pawn = this.nest();
     this.onPath.push(pawn);
@@ -115,20 +72,21 @@ function game(){ //LOGIC KEEP ALL PLAY INSIDE GAME FUNCTION WITH NESTED ARRAY / 
 
   }
  
-  function openingRollofDie(){
+  openingRollofDie = () => {
     let dieWinner;
     let playerRoll = Math.floor((Math.random()*6)+1);
     let computerRoll = Math.floor((Math.random()*6)+1);
+    console.log(dice1, dice2);
     if (playerRoll < computerRoll){
       dieWinner = player;
-      console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${dieWinner} plays first.`);
+      console.log(`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${dieWinner} plays first.`);
     }
     else if (playerRoll > computerRoll) {
       dieWinner = computer;
-      console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${dieWinner} plays first.`);
+      console.log(`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. ${dieWinner} plays first.`);
       }
     else {
-      console.log (`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. We don't have a winner. Roll again, and let's see who goes first.`);
+      console.log(`${player} rolls ${playerRoll}`+ " and " + `${computer} rolls ${computerRoll}. We don't have a winner. Roll again, and let's see who goes first.`);
         //rollDie(); this code works but the automation is instant without letting the player interact //BUTTON THAT SAYS ROLL AGAIN?
     }
 
@@ -207,11 +165,12 @@ var blueplayerLocation = 0;
 var yellowplayerLocation = 0;
 
 const rollTwoDiceBlue= ()=>{
-  const dice = Math.floor(Math.random() * 6 + 1)
+  const dice = firstRandomNum;
+  let firstDiceImage = firstRandomNum;
   document.querySelector(`#div${blueplayerLocation}`).classList.remove("bluecircle");
   blueplayerLocation += dice
   console.log(blueplayerLocation)
-  if(blueplayerLocation >15){
+  if(blueplayerLocation > 40){
     alert('The player has won!')
     return
   }
@@ -220,11 +179,12 @@ const rollTwoDiceBlue= ()=>{
 }
 
 const rollTwoDiceYellow= ()=>{
-  const dice = Math.floor(Math.random() * 6 + 1)
+  const dice = secondRandomNum;
+  let secondDiceImage = secondRandomNum;
   document.querySelector(`#div${yellowplayerLocation}`).classList.remove("yellowcircle");
   yellowplayerLocation += dice
   console.log(yellowplayerLocation)
-  if(yellowplayerLocation >15){
+  if(yellowplayerLocation >40){
     alert('The computer has won!')
     return
   }
